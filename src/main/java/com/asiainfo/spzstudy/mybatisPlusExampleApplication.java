4a9c7d96c015aa4348775db687ad9d0582e0854d
songpeizhe <songpeizhe@utry.cn>
2022-01-06 09:38:28 +0800

    修改名称

--- src/main/java/com/asiainfo/spzstudy/mybatisPlusExampleApplication.java
+++ src/main/java/com/asiainfo/spzstudy/mybatisPlusExampleApplication.java
@@ -3,17 +3,15 @@ package com.asiainfo.spzstudy;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("com.asiainfo.spzstudy.mapper")
-public class SpzStudyApplication {
+public class mybatisPlusExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpzStudyApplication.class, args);
+        SpringApplication.run(mybatisPlusExampleApplication.class, args);
     }
 
 }
